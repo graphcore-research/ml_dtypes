4bb337e3a76c9ee2c0fe72e932020d324aaf6e9e
Paul Balanca <paulb@graphcore.ai>
2024-07-31 16:23:51 +0100

    Add float8_e8m0_fnu (E8M0) OCP MX scale format
    Adding the OCP MX scale format `E8M0`, which has the following properties:
    * Unsigned format;
    * 8 exponent bits;
    * Exponent range from -127 to 127;
    * No zero and infinity;
    * Single NaN value (0xFF);
    
    `ml_dtypes` `float8_base` C++ class is extended to support floating point formats
    which are unsigned and with no zero (i.e. additional `kIsSigned` and `kHasZero` Traits properties).
    
    Base on these traits, `float8_e8m0_fnu` has been implemented using the existing functionalities (convert, unary/binary ops, ...).
    Float8 Python unit tests have been extended to be able to cover unsigned floating point formats.

--- ml_dtypes/__init__.py
+++ ml_dtypes/__init__.py
@@ -24,6 +24,7 @@ __all__ = [
     "float8_e4m3fnuz",
     "float8_e5m2",
     "float8_e5m2fnuz",
+    "float8_e8m0fnu",
     "iinfo",
     "int2",
     "int4",
@@ -43,6 +44,7 @@ from ml_dtypes._ml_dtypes_ext import float8_e4m3fn
 from ml_dtypes._ml_dtypes_ext import float8_e4m3fnuz
 from ml_dtypes._ml_dtypes_ext import float8_e5m2
 from ml_dtypes._ml_dtypes_ext import float8_e5m2fnuz
+from ml_dtypes._ml_dtypes_ext import float8_e8m0fnu
 from ml_dtypes._ml_dtypes_ext import int2
 from ml_dtypes._ml_dtypes_ext import int4
 from ml_dtypes._ml_dtypes_ext import uint2
@@ -57,6 +59,7 @@ float8_e4m3fn: Type[np.generic]
 float8_e4m3fnuz: Type[np.generic]
 float8_e5m2: Type[np.generic]
 float8_e5m2fnuz: Type[np.generic]
+float8_e8m0fnu: Type[np.generic]
 int2: Type[np.generic]
 int4: Type[np.generic]
 uint2: Type[np.generic]
